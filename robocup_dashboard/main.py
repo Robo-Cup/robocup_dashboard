a0f033b1eec1ed47e51082cd66675fb56ce78eff
LavaWaffle <bjxobjxo@gmail.com>
2023-12-23 17:50:59 -0500

    Add subscribed value functionality to DashboardNode

--- robocup_dashboard/main.py
+++ robocup_dashboard/main.py
@@ -1,8 +1,9 @@
 #!env/usr/bin python3
 import rclpy
+from rclpy.action import ActionServer
 from rclpy.node import Node
 
-from dashboard_interfaces.srv import SetDashboardKeys
+from dashboard_interfaces.srv import SetDashboardKeys, SetSubscribedValues
 from dashboard_interfaces.msg import DashboardValues
 
 from robocup_dashboard.dashboard.exceptions import DashboardException
@@ -20,6 +21,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 class DashboardNode(Node):
+    subscribed_value_indexes: list[int] = []
     def __init__(self):
         # Ros2 initialization
         super().__init__('dashboard_node')
@@ -27,7 +29,9 @@ class DashboardNode(Node):
 
         # Ros2 set up services
         self.set_dashboard_keys_service = self.create_service(SetDashboardKeys, 'set_dashboard_keys', self.set_dashboard_keys_callback)
-        self.dashboard_values_subscriber = self.create_subscription(DashboardValues, 'dashboard_values', self.dashboard_values_callback, 10)
+        self.set_subscribed_values_service = self.create_service(SetSubscribedValues, 'set_subscribed_values', self.subscribe_to_values_callback)
+        self.dashboard_values_subscriber = self.create_subscription(DashboardValues, 'set_dashboard_values', self.dashboard_values_callback, 10)
+        self.dashboard_values_publisher = self.create_publisher(DashboardValues, 'get_dashboard_values', 10)
 
         # Dashboard initialization
         self.dashboard_gui: DashboardGui = DashboardGui()
@@ -35,6 +39,8 @@ class DashboardNode(Node):
         # Start the pygame loop with ros2 timer
         self.refresh_rate = 60
         self.timer_ = self.create_timer(1.0 / self.refresh_rate, self.dashboard_run)
+
+        self.subscribed_value_timer = self.create_timer(1.0, self.subscribed_value_timer_callback)
 
     def dashboard_values_callback(self, msg: DashboardValues):
         self.dashboard_gui.set_values(msg.key_indexes, msg.key_values)
@@ -46,8 +52,31 @@ class DashboardNode(Node):
 
         return response
 
+    def subscribe_to_values_callback(self, request: SetSubscribedValues.Request, response):
+        self.subscribed_value_indexes = request.key_indexes
+        self.get_logger().info(f"Dashboard subscribed to values: {request.key_indexes}")
+
+        return response
+
+    def get_values(self, key_indexes: list) -> list[float]:
+        ret_val = []
+        for index in key_indexes:
+            # check if index is out of bounds
+            if index >= len(self.dashboard_gui.values):
+                ret_val.append(0.0)
+            else:
+                ret_val.append(float(self.dashboard_gui.values[index]))
+        return ret_val
+
     def dashboard_run(self):
         self.dashboard_gui.run()
+
+    def subscribed_value_timer_callback(self):
+        if len(self.subscribed_value_indexes) > 0:
+            msg = DashboardValues()
+            msg.key_indexes = self.subscribed_value_indexes
+            msg.key_values = self.get_values(self.subscribed_value_indexes)
+            self.dashboard_values_publisher.publish(msg)
 
 
 def main(args=None):
